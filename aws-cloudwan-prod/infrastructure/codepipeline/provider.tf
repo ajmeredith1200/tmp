0e814721a9a8d77f9c6217b0ce578d8c246e98a0
Alex Meredith <100841449+ajmeredith1200@users.noreply.github.com>
2026-09-18 09:35:23 +0100

    Delete aws-cloudwan-prod directory

--- aws-cloudwan-prod/infrastructure/codepipeline/provider.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "aws-cloudwan-terraform-state"
-    key          = "prod/codepipeline.tfstate"
-    region       = "eu-west-1"
-    encrypt      = true
-    use_lockfile = true
-  }
-}
